7f05059a3c1fe13a7dcf18b34becdc291bcb177b
NinaThanina <nina.naitatmane@gmail.com>
2017-10-18 15:27:34 +0200

    Commentaires

--- problems.py
+++ problems.py
@@ -2,9 +2,12 @@ import numpy as np
 
 class Problem():
 	def __init__(self, f, d, low, high):
-		self.f = f		
+		self.f = f
+		//dimensions du vecteur 		
 		self.d = d
+		//la plus petite valeur de lintevalle des valeurs du vecteur
 		self.low = low
+		//la plus grande valeur de lintevalle des valeurs du vecteur
 		self.high = high
 
 	
