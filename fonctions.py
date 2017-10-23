4fb2c94c43321fdd7c9658937028de96707ffc26
NinaThanina <nina.naitatmane@gmail.com>
2017-10-23 13:37:21 +0200

    aa

--- fonctions.py
+++ fonctions.py
@@ -1,9 +1,12 @@
 import numpy as np
 
 """
-Ces fonctions doivent renvoyer une valeur normalisée dans l'intervalle [0,1].
+Ces fonctions doivent renvoyer une valeur normalisee dans lintervalle [0,1].
 
 """
 
 def sphere(solution):
 	return np.sum(np.square(solution))
+
+
+
